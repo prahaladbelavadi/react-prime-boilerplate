1d3f4f82d6d9efd4d506a393fcd6212d20e73721
prahaladbelavadi-anvil <github@belavadi.com>
2022-10-23 23:04:11 -0700

    initial commit boilerplat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
+      <br />
+      <Button label='Submit' />
+      <br/>
+      <i className="pi pi-check mr-2"></i>
+      <i className="pi pi-times"></i>
+
     </div>
   );
 }
